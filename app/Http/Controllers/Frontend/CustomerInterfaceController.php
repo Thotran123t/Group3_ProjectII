e25b3247a7d0bb69d61eea62cea34b9a895451d1
NguyenHoangAn31 <anphung311002@gmail.com>
2023-04-13 09:16:31 +0700

    up .env.example file

--- app/Http/Controllers/Frontend/CustomerInterfaceController.php
+++ app/Http/Controllers/Frontend/CustomerInterfaceController.php
@@ -5,41 +5,48 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class CustomerInterfaceController extends Controller
 {
-    public function home(){
+    public function home()
+    {
         return view('frontend/home');
     }
-    public function shop(){
+    public function shop()
+    {
         return view('frontend/shop');
     }
-    public function blog(){
+    public function blog()
+    {
         return view('frontend/blog');
     }
-    public function about(){
+    public function about()
+    {
         return view('frontend/about');
     }
-    public function contact(){
+    public function contact()
+    {
         return view('frontend/contact');
     }
-    public function cart(){
-        if(session()->has('user')){
+    public function cart()
+    {
+        if (session()->has('user')) {
             return view('frontend/cart');
-        }
-        else{
+        } else {
             return view('frontend/sign_in');
         }
     }
-    public function sign_in(){
+    public function sign_in()
+    {
         return view('frontend/sign_in');
     }
-    public function sign_up(){
+    public function sign_up()
+    {
         return view('frontend/sign_up');
     }
-
-
-    public function create_user(Request $request){
+    public function create_user(Request $request)
+    {
         $customer = Customer::firstOrCreate(['email' => $request->email], [
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
@@ -48,8 +55,24 @@ class CustomerInterfaceController extends Controller
             'address' => $request->address,
             'password' => $request->password,
         ]);
-    }
-    public function signin_user(Request $request){
 
+        if ($customer->wasRecentlyCreated) {
+            $name = $request->first_name . ' ' . $request->last_name;
+    
+            
+            Mail::send('frontend.sendmail', compact('name'), function ($email) use ($request) {
+                $email->to($request->email)->subject('Email from IShopApple');
+            });
+            session()->put('user', $customer);
+
+            return redirect('frontend/home');
+
+        } else {
+            return redirect('frontend/create_user');
+        }
+    }
+
+    public function signin_user(Request $request)
+    {
     }
 }
